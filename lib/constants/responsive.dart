6aa48b7c4a111d5d233513525a0669c5875f31b9
Guerkan-Oeztuerk <guerkan.oeztuerk@bosch.com>
2022-10-26 18:47:36 +0200

    style

--- lib/constants/responsive.dart
+++ lib/constants/responsive.dart
@@ -12,7 +12,6 @@ class Responsive extends StatelessWidget {
     required this.desktop,
   }) : super(key: key);
 
-
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < 850;
 
@@ -28,11 +27,9 @@ class Responsive extends StatelessWidget {
     final Size _size = MediaQuery.of(context).size;
     if (_size.width >= 1100) {
       return desktop;
-    }
-    else if (_size.width >= 850 && tablet != null) {
+    } else if (_size.width >= 850 && tablet != null) {
       return tablet!;
-    }
-    else {
+    } else {
       return mobile;
     }
   }
