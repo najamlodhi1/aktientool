35f3734fd15a5642244d169411b63adb8fee948f
guerkan-oeztuerk <guerkan.oeztuerk@bosch.com>
2022-12-01 15:04:00 +0100

    Bugfix Filter functions

--- lib/stockscreener/home.dart
+++ lib/stockscreener/home.dart
@@ -1,6 +1,10 @@
-import 'package:aktientool/filter/showCompanies.dart';
+import 'package:aktientool/filter/showCompany2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../filter/country.dart';
+import '../filter/industry.dart';
+import '../filter/marketcap.dart';
 import '../filter/searcharea.dart';
 
 class Home extends ConsumerWidget {
@@ -8,6 +12,12 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    int marketcap = ref.watch(sp_marketcap);
+    List<dynamic> generatedCountrieFromList =
+        ref.watch(sp_generatedCountrieFromList);
+    List<dynamic> generatedIndustryFromList =
+        ref.watch(sp_generatedIndustryFromList);
+
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       appBar: SearchArea(),
