08d15a5b3cca037abbcf1f1a7c961ac06144b6ac
guerkan-oeztuerk <guerkan.oeztuerk@bosch.com>
2023-01-04 19:42:06 +0100

    Stripe

--- lib/payment/stripe/stripe_checkout_web.dart
+++ lib/payment/stripe/stripe_checkout_web.dart
@@ -18,6 +18,8 @@ void redirectToCheckout(BuildContext context) async {
       .doc(userUid)
       .collection('checkout_sessions')
       .add({
+    //'price': pd.priceId,
+    'quantity': 1,
     'mode': 'payment',
     'success_url': 'https://aktientool.net',
     'cancel_url': 'https://aktientool.net'
@@ -80,6 +82,7 @@ void redirectToCheckout(BuildContext context) async {
       }
     }
   });
+}
 
 /*
   try {
@@ -106,7 +109,6 @@ void redirectToCheckout(BuildContext context) async {
   }
 
   */
-}
 
 @JS()
 class Stripe {
