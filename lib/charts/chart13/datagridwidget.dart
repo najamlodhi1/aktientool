f0fbee4cfd5746e0d9f41c8b6eb17514e7525770
apple <apple@apples-MacBook-Pro.local>
2023-02-21 10:31:00 -0800

    back button

--- lib/charts/chart13/datagridwidget.dart
+++ lib/charts/chart13/datagridwidget.dart
@@ -18,6 +18,39 @@ class DataGridWidget extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Column(
         children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.centerLeft,
+            child: IconButton(
+              icon: const Icon(
+                Icons.keyboard_arrow_left,
+                size: 50,
+                color: Colors.black,
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.centerLeft,
+            child: Container(
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: MaterialButton(
+                child: const Text(
+                  'Back',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ),
           const Text(
             'IPO Calendar',
             style: TextStyle(
@@ -31,16 +64,17 @@ class DataGridWidget extends StatelessWidget {
             child: DataTable(
                 headingRowColor: MaterialStateProperty.all(Colors.grey[300]),
                 columns: const [
+                  DataColumn(label: Text('Date')),
                   DataColumn(label: Text('Company')),
                   DataColumn(label: Text('Symbol')),
                   DataColumn(label: Text('Exchange')),
-                  DataColumn(label: Text('Date')),
                   DataColumn(label: Text('PriceRange')),
                 ],
                 rows: [
                   ...companyInfoList.map((e) {
                     final date = DateFormat('MMM dd yyyy').format(e.date);
                     return DataRow(cells: [
+                      DataCell(Text(date)),
                       DataCell(Text(e.company), onTap: () {
                         html.window.open(
                             'https://www.google.com/search?q=${e.company}',
@@ -48,7 +82,6 @@ class DataGridWidget extends StatelessWidget {
                       }),
                       DataCell(Text(e.symbol)),
                       DataCell(Text(e.exchange)),
-                      DataCell(Text(date)),
                       DataCell(Text(e.priceRange.toString())),
                     ]);
                   }).toList(),
