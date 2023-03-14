afffa184d938887724aae48c206fbfe9d796f80e
guerkan-oeztuerk <guerkan.oeztuerk@bosch.com>
2023-03-14 16:18:18 +0100

    add   payment_method_types: ['card', 'sepa_debit'],

--- lib/payment/stripe/hompage.dart
+++ lib/payment/stripe/hompage.dart
@@ -54,6 +54,7 @@ class _HomepageState extends State<Homepage> {
       'price': pd.priceId,
       'quantity': pd.quatity,
       'mode': 'payment',
+      'payment_method_types': ['card', 'sepa_debit'],
       'success_url': 'https://aktientool.net/?ret=success&q=$clicks',
       'cancel_url': 'https://aktientool.net/?ret=cancel'
     });
