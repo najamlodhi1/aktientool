c503661f2728d7bc51192581a0d11c87e4c99364
guerkan-oeztuerk <guerkan.oeztuerk@bosch.com>
2023-04-16 21:53:31 +0200

    change website

--- lib/webpage/components/ios_app_ad.dart
+++ lib/webpage/components/ios_app_ad.dart
@@ -37,7 +37,7 @@ class IosAppAd extends StatelessWidget {
                   Expanded(
                     flex: constraints.maxWidth > 720.0 ? 1 : 0,
                     child: Image.asset(
-                      "assets/ios.png",
+                      "assets/images/website.png",
                       // Set width for image on smaller screen
                       width: constraints.maxWidth > 720.0 ? null : 350.0,
                     ),
